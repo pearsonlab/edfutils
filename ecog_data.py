590520e52f7f73ec5bdea400fe79f7c8919f06c2
shariqiqbal2810 <shariqiqbal2810@users.noreply.github.com>
2016-02-09 17:54:00 -0500

    Added skeleton for ECOGData class that will act as intermediary betwwen formats

---
+++ ecog_data.py
@@ -0,0 +1,16 @@
+
+class ECOGData:
+    """
+    This class will act as a convenient holder for ECoG data
+
+    TO-DO:
+    * from_edf: function that will load an edf file into the class
+        * This can use methods from edf_split.py
+    * from_open_ephys: function that will load data from open ephys
+    * to_chn: function that will write .chn files for each channel
+    * from_chn: function that will read folder of .chn files
+    * to_hdf5: function that will write all channels to a single hdf5 file
+    * from_hdf5: function that will read hdf5 file
+    """
+    def __init__(self):
+        pass
